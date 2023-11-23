25212d5c0ea1683a3c0ee4164e59f1744fb661f5
AlexForget <alexandreqc26@gmail.com>
2023-11-23 09:04:00 -0500

    add transition between level

--- scenes/levels/outside.gd
+++ scenes/levels/outside.gd
@@ -1,9 +1,12 @@
 extends LevelParent
 
+#var inside_level_scene: PackedScene = preload("res://scenes/levels/inside.tscn")
 
 func _on_gate_player_entered_gate(_body):
 	var tween = create_tween()
 	tween.tween_property($Player, "speed", 0, 0.4)
+	get_tree().change_scene_to_file("res://scenes/levels/inside.tscn")
+#	get_tree().change_scene_to_packed(inside_level_scene)
 
 
 func _on_house_player_entered():
