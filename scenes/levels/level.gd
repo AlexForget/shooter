7e4a2badda83b65efa1c722e89d993c47069d6a9
AlexForget <alexandreqc26@gmail.com>
2023-11-22 22:36:50 -0500

    inheritance and inside level

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,11 +1,8 @@
 extends Node2D
+class_name LevelParent
 
 var laser_scene: PackedScene = preload("res://scenes/projectiles/laser.tscn")
 var grenade_scene: PackedScene = preload("res://scenes/projectiles/grenade.tscn")
-
-func _on_gate_player_entered_gate(_body):
-	var tween = create_tween()
-	tween.tween_property($Player, "speed", 0, 0.4)
 
 
 func _on_player_laser_is_use(pos, player_direction):
@@ -23,11 +20,4 @@ func _on_player_grenade_is_use(pos, player_direction):
 	$Projectile.add_child(grenade)
 
 
-func _on_house_player_entered():
-	var tween = get_tree().create_tween()
-	tween.tween_property($Player/Camera2D, "zoom", Vector2(0.6, 0.6), 1)
 
-
-func _on_house_player_exited():
-	var tween = get_tree().create_tween()
-	tween.tween_property($Player/Camera2D, "zoom", Vector2(0.4, 0.4), 2)
