8f706484c41566e64133258a3f9257f851ad72d0
AlexForget <alexandreqc26@gmail.com>
2023-12-01 08:17:34 -0500

    Create bug scene + area and collision

---
+++ scenes/enemys/bug.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+var active: bool = false
+var speed: int = 300
+var vulnerable: bool = true
+var player_near: bool = false
+
+
+func _on_notice_area_body_entered(body):
+	pass # Replace with function body.
+
+
+func _on_notice_area_body_exited(body):
+	pass # Replace with function body.
+
+
+func _on_attack_area_body_entered(body):
+	pass # Replace with function body.
+
+
+func _on_attack_area_body_exited(body):
+	pass # Replace with function body.
