8251836d4a524d3088ae3ec519bad890c34d43c5
AlexForget <alexandreqc26@gmail.com>
2023-12-09 11:30:54 -0500

    Add life, attack and activation to the animation

--- scenes/enemys/hunter.gd
+++ scenes/enemys/hunter.gd
@@ -2,8 +2,13 @@ extends CharacterBody2D
 
 var active: bool = false
 var speed: int = 200
+var player_near: bool = false
+var vulnerable: bool = true
+var health: int = 100
 
 func _ready():
+	$NavigationAgent2D.path_desired_distance = 4.0
+	$NavigationAgent2D.target_desired_distance = 4.0
 	$NavigationAgent2D.target_position = Globals.player_position
 	
 
@@ -16,18 +21,43 @@ func _physics_process(_delta):
 		var direction: Vector2 = (next_path_position - global_position).normalized()
 		velocity = direction * speed
 		move_and_slide()
-		look_at(Globals.player_position)
+		var look_angle = direction.angle()
+		rotation = look_angle + PI / 2
 
 func _on_notice_area_body_entered(_body):
 	active = true
-	print(active)
+	$AnimationPlayer.play("walk")
 
 
 func _on_notice_area_body_exited(_body):
 	active = false
-	print(active)
 
 
 func _on_navigation_timer_timeout():
 	if active:
 		$NavigationAgent2D.target_position = Globals.player_position
+
+
+
+func _on_attack_area_body_entered(_body):
+	player_near = true
+	$AnimationPlayer.play("attack")
+
+
+func _on_attack_area_body_exited(_body):
+	player_near = false
+
+
+func attack():
+	if (player_near):
+		Globals.health -= 20
+
+func hit():
+	if vulnerable:
+		health -= 10
+		$Timers/HitTimer.start()
+	if health <= 0:
+		queue_free()
+
+func _on_hit_timer_timeout():
+	vulnerable = true
